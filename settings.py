64c0f58fd13192d2af96b248b0e587f657f420b7
Assassi, Youness (ya2257) <ya2257@us.att.com>
2015-10-17 20:31:19 -0400

    Initial commit

---
+++ settings.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+"""settings.py
+
+Udacity conference server-side Python App Engine app user settings
+
+$Id$
+
+created/forked from conference.py by wesc on 2014 may 24
+
+"""
+
+# Replace the following lines with client IDs obtained from the APIs
+# Console or Cloud Console.
+WEB_CLIENT_ID = '987715460853-tvcrimhc1hvgso29evg09l5a5tf9ht3o.apps.googleusercontent.com'
